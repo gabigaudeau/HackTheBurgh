7c15afce89735d387ff1b989ee3dd92627ba9773
HeadOfTheOrder <48604195+HeadOfTheOrder@users.noreply.github.com>
2019-03-17 22:21:22 +0000

    checking test

--- faces.py
+++ faces.py
@@ -1,9 +1,14 @@
 # import the libraries
+import nltk
 import os
 import face_recognition
 import numpy as np
 from natsort import natsorted
 from scraper import formatProcess
+from roast_to_poem import main
+
+
+
 #absolute bath
 def roast(imgpath):
     #matches = os.listdir('unknown_face')
@@ -11,20 +16,23 @@ def roast(imgpath):
     #for match in matches:
 #        image_to_be_matched = face_recognition.load_image_file('unknown_face/' + match)
 #    image_to_be_matched_encoded = face_recognition.face_encodings(image_to_be_matched)[0]
-    image_to_be_matched_encoded = face_recgonition.face_encoding(imgpath)
+    image_to_be_matched = face_recognition.load_image_file(imgpath)
+    image_to_be_matched_encoded = face_recognition.face_encodings(image_to_be_matched)
         # iterate over each encodings
     known_roasts=natsorted(os.listdir('trainingset/text'))
     encodings=[]
-    distances = None
     files=natsorted(os.listdir('encodings'))
     for index, file in enumerate(files):
         readFile=open('encodings/'+file, "r", encoding="utf-8")
         if os.stat('encodings/'+file).st_size==0:
-            encodings.append(np.zeros(128))
+            encodings.append(np.zeros((128,)))
             continue
         fileContents=readFile.read()
-        encodings.append((np.fromstring(fileContents[1:-1].replace('\n',''), sep=' ')))
-    distances = face_recognition.face_distance(encodings, image_to_be_matched_encoded)
+
+        encodings.append(np.fromstring(fileContents[1:-1].replace('\n',''), sep=' '))
+    distances = []
+    for encoding in encodings:
+        distances.append(face_recognition.face_distance(encoding, image_to_be_matched_encoded))
 
     lowestDist= distances[0]
     lowestIndex=0
@@ -34,7 +42,11 @@ def roast(imgpath):
             lowestIndex=index
         else:
             continue
-
-    rightRoastFile=open('trainingSet/text/' + known_roasts[lowestIndex], encoding="utf-8")
-    return rightRoastFile.readLine()
-    #roast()
+    path = os.getcwd()+ "/"
+    line = ""
+    with open('trainingSet/text/' + known_roasts[lowestIndex], encoding="utf-8") as f:
+        content = f.read()
+    line = content.split('\n')[0]
+    print(line)
+    return line #main(formatProcess(line),"C:/Users/Lars Thalian/Documents/GitHub/HackTheBurgh/trainingSet/formatedRoasts/roastDatabase.txt","C:/Users/Lars Thalian/Documents/GitHub/HackTheBurgh/phodict.txt")
+#roast(os.getcwd()+ "/unknown_face/roast15.jpg" )

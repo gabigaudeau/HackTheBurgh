f48f55318527e251054235e7c9e816137ffcafa2
Gabrielle Gaudeau <43742635+theHappyCarrot@users.noreply.github.com>
2019-03-17 07:05:04 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, request, jsonify
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__, template_folder="templates")
+
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('roast_me.html')
+
+@app.route('/uploads', methods=['POST'])
+def upload_file():
+    file = request.files['image']
+    file.save(secure_filename(file.filename))
+    image_path = os.path.join(app.instance_path, "/uploads", file.filename)
+    poem = image_path
+    return render_template('result.html', value=poem)
+app.run(debug=True)

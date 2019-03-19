390ee4a4471cc5dac7b92b248f91dc103a2f25f6
HeadOfTheOrder <48604195+HeadOfTheOrder@users.noreply.github.com>
2019-03-19 22:58:21 +0000

    Add files via upload
    Added webcam roast and cleaned up faces.py

--- faces.py
+++ faces.py
@@ -1,40 +1,46 @@
 # import the libraries
+import nltk
 import os
 import face_recognition
 import numpy as np
 from natsort import natsorted
 from scraper import formatProcess
-#absolute bath
+from roast_to_poem import main
+
+
+# absolute bath
 def roast(imgpath):
-    #matches = os.listdir('unknown_face')
-    #image_to_be_matched = None
-    #for match in matches:
-#        image_to_be_matched = face_recognition.load_image_file('unknown_face/' + match)
-#    image_to_be_matched_encoded = face_recognition.face_encodings(image_to_be_matched)[0]
-    image_to_be_matched_encoded = face_recgonition.face_encoding(imgpath)
-        # iterate over each encodings
-    known_roasts=natsorted(os.listdir('trainingset/text'))
-    encodings=[]
-    distances = None
-    files=natsorted(os.listdir('encodings'))
+
+    image_to_be_matched = face_recognition.load_image_file(imgpath)
+    image_to_be_matched_encoded = face_recognition.face_encodings(image_to_be_matched)
+    known_roasts = natsorted(os.listdir('trainingset/text'))
+    encodings = []
+    files = natsorted(os.listdir('encodings'))
     for index, file in enumerate(files):
         readFile=open('encodings/'+file, "r", encoding="utf-8")
-        if os.stat('encodings/'+file).st_size==0:
-            encodings.append(np.zeros(128))
+        if os.stat('encodings/'+file).st_size == 0:
+            encodings.append(np.zeros((128,)))
             continue
-        fileContents=readFile.read()
-        encodings.append((np.fromstring(fileContents[1:-1].replace('\n',''), sep=' ')))
-    distances = face_recognition.face_distance(encodings, image_to_be_matched_encoded)
+        fileContents = readFile.read()
 
-    lowestDist= distances[0]
-    lowestIndex=0
+        encodings.append(np.fromstring(fileContents[1:-1].replace('\n', ''), sep=' '))
+    distances = []
+    for encoding in encodings:
+        distances.append(face_recognition.face_distance(encoding, image_to_be_matched_encoded))
+
+    lowestDist = distances[0]
+    lowestIndex = 0
     for index, distance in enumerate(distances):
-        if(distance<lowestDist):
-            lowestDist=distance
-            lowestIndex=index
+        if(distance < lowestDist):
+            lowestDist = distance
+            lowestIndex = index
         else:
             continue
-
-    rightRoastFile=open('trainingSet/text/' + known_roasts[lowestIndex], encoding="utf-8")
-    return rightRoastFile.readLine()
-    #roast()
+    path = os.getcwd() + "/"
+    with open('trainingSet/text/' + known_roasts[lowestIndex], encoding="utf-8") as f:
+        content = f.read()
+    line = content.split('\n')[0]
+    print(line)
+    return line
+    # main(formatProcess(line),"C:/Users/Lars Thalian/Documents/GitHub/HackTheBurgh/trainingSet/formatedRoasts/roastDatabase.txt","C:/Users/Lars Thalian/Documents/GitHub/HackTheBurgh/phodict.txt")
+    # roast(os.getcwd()+ "/unknown_face/roast15.jpg" )

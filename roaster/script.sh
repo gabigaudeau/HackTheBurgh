57c060733be65f6d5198c30b673898fe92ac61ba
makspll <makspl17@gmail.com>
2019-04-04 15:56:28 +0100

    asd

--- roaster/script.sh
+++ roaster/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source venv/bin/activate >> log.txt
-nice python3 databaseExtender.py >> log.txt
-echo $'\r' >> log.txt
+source venv/bin/activate > log.txt
+nice python3 databaseExtender.py > log.txt
+echo $'\r' > log.txt
